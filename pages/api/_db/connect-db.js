d525c56b3ae7891626cd031964723248c3836c84
Melih Firat <mfirat.medien@web.de>
2022-12-26 01:48:55 +0100

    fixes

--- pages/api/_db/connect-db.js
+++ pages/api/_db/connect-db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = handler => async (req, res) => {
-  if (!process.env.MONGO_DB_URI) {
+  if ((!process.env.MONGO_DB_URI, !process.env.MONGO_DB_URI2)) {
     throw new Error("Please set environment variable MONGO_DB_URI.");
   }
   if (mongoose.connections[0].readyState) {
@@ -9,7 +9,7 @@ const connectDB = handler => async (req, res) => {
     return handler(req, res);
   }
   // Use new db connection
-  await mongoose.connect(process.env.MONGO_DB_URI);
+  await mongoose.connect(process.env.MONGO_DB_URI, !process.env.MONGO_DB_URI2);
   return handler(req, res);
 };
 
